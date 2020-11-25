73fae87f89319d1e9f46a7459cc08e3035c2ee73
ren <ren_fujiwara@serio-toyo.co.jp>
2020-11-25 11:10:10 +0900

    Heroku用セットアップ

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
-  production: true
+  production: true,
+  limitPage: 5, // 指定回数(1ページに表示する項目の数)を定義
+  listPlace:[// 場所の一覧
+    { name : "ミーティングルーム1" },
+    { name : "ミーティングルーム2" },
+    { name : "ミーティングルーム4" },
+    { name : "応接室" },
+    { name : "特別会議室" },
+    { name : "休憩室" },
+    { name : "roomR" },
+    { name : "roomB" },
+    { name : "roomY" },
+    { name : "roomG" }
+  ]
 };
